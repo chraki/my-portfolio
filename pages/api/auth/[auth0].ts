4d02ee21022a8db0364e8d487b6247193db61b37
chraki <rakeysh325@gmail.com>
2023-11-06 17:44:46 +0530

    changes

---
+++ pages/api/auth/[auth0].ts
@@ -0,0 +1,12 @@
+import { handleAuth, handleLogout, handleCallback } from '@auth0/nextjs-auth0';
+
+export default handleAuth({
+    async logout(req, res) {
+        try {
+          await handleLogout();
+        } catch (error) {
+            console.log(error)
+        //   res.status(error.status || 400).end(error.message);
+        }
+      },
+});
