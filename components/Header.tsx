b554eb3576d4d19d7092469b7f12a9ede4544b51
chraki <rakeysh325@gmail.com>
2023-11-06 18:02:15 +0530

    changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,8 +21,8 @@ const Header:React.FC<Props> = (props: any) => {
         <li onClick={() => setToggle('Projects')} className={styles.sections}>Projects</li>
         <li onClick={() => setToggle('Contact')} className={styles.sections}>Contact</li>
         <li className={styles.sections}>
-      <Link className={styles.sections} href="/api/auth/login"><a>Login</a></Link>
-      </li>
+      <Link className={styles.sections} href="/api/auth/login">Login</Link>
+      </li> 
       {/* <li className={styles.sections}><a className={styles.sections} href="/api/auth/logout">Logout</a></li> */}
       </ul>
     </div>
